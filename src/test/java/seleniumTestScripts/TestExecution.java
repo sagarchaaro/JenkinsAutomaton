d51dc11b07cd297d8ac5beae521dc7c4d61c7a1f
SagarCH <sagarch.aaro@gmail.com>
2021-02-21 23:26:05 +0530

    change in the logic

--- src/test/java/seleniumTestScripts/TestExecution.java
+++ src/test/java/seleniumTestScripts/TestExecution.java
@@ -12,5 +12,20 @@ public class TestExecution {
 		System.out.println("Application Name is :"+applicationName);
 		System.out.println("userName Name is :"+userName);
 		System.out.println("password Name is :"+password);
+		
+		if(applicationName.equalsIgnoreCase("Facebook")) {
+			int a=10;
+			if(a%2==0) {
+				System.out.println("Give number is even");
+			}else {
+				System.out.println("Given number is odd");
+			}
+		}else if(applicationName.equalsIgnoreCase("Google")) {
+			int a=10, b=20;
+			System.out.println(a+b);
+		}else {
+			int a=5;
+			System.out.println("Cube of the given number is:"+a);
+		}
 	}
 }
